05ebd37f7cc41f9e1bf0f8d85cd687cb202e989c
Gaeus <skypol59@msn.com>
2025-01-08 16:39:10 +0100

    working cheat sheet regarding python calculation

---
+++ Maths.py
@@ -0,0 +1,11 @@
+# Write your solution here
+input_number1 = int(input("Number 1:"))
+input_number2 = int(input("Number 2:"))
+input_number3 = int(input("Number 3:"))
+input_number4 = int(input("Number 4:"))
+sum = input_number1 + input_number2 + input_number3 + input_number4
+product = input_number1 * input_number2 * input_number3 * input_number4
+mean = sum / 4
+print (f"The sum of the numbers: {sum}")
+print (f"The product of the numbers: {input_number1 * input_number2 * input_number3 * input_number4}")
+print (f"The mean of the numbers: {mean}")
