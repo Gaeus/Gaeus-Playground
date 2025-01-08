46bf83e5a4c8e4b7217b66af3eb778248b2f33a7
Gaeus <skypol59@msn.com>
2025-01-08 17:28:20 +0100

    order of magnitude

---
+++ magnitude.py
@@ -0,0 +1,9 @@
+numberinput=int(input("Please type in a number:"))
+
+if numberinput < 1000:
+    print("This number is smaller than 1000")
+if numberinput < 100:
+    print("This number is smaller than 100")
+if numberinput < 10:
+    print("This number is smaller than 10")
+print("Thank you!")
