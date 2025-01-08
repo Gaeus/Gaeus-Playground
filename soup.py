751420f6b6b03f8055f6d735a882c15de0c3b240
Gaeus <skypol59@msn.com>
2025-01-08 17:06:57 +0100

    discrimination engine

---
+++ soup.py
@@ -0,0 +1,20 @@
+name=input("Please tell me your name: ")
+soupcost=5.9
+if name == "Jerry":
+    print("Next please!")
+
+elif name == "jerry":
+    print("Next please!")
+
+elif name == "JERRY":
+    print("Next please!")
+
+else:
+    soupinput=input("How many portions of soup? ")
+    print(f"the total cost is : {int(soupinput)*soupcost}")
+
+
+
+
+
+
