94ee05835e314af51baa0421e071bb9d5c0a6b1c
Gaeus <skypol59@msn.com>
2025-01-10 10:51:48 +0100

    fixed WTW

--- WTW.py
+++ WTW.py
@@ -2,12 +2,12 @@ print("What's the weather forecast for tomorrow?")
 temperature=float(input("Temperature:"))
 rain=input("Will it rain tomorrow? (yes/no):").lower()
 print("Wear jeans and a T-shirt")
-if temperature<20:
+if temperature<=20:
   print("I recommend a jumper as well")
-if temperature<10:
+if temperature<=10:
   print("Take a jacket with you")
-if temperature<5:
-    print("Make it a waarm coat, actually")
+if temperature<=5:
+    print("Make it a warm coat, actually")
     print("I think gloves are in order")
 if rain=="yes":
-    print("Don't forget your umbrella")
+    print("Don't forget your umbrella!")
